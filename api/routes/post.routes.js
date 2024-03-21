cc1c66e5f40593e8452397e486c5e10c9cb4bf8f
Anji-fdo <131850714+Anji-fdo@users.noreply.github.com>
2024-03-21 10:42:28 +0530

    add post section and create get posts api route

--- api/routes/post.routes.js
+++ api/routes/post.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { verifyToken } from '../utils/veriyUser.js';
-import { create } from '../controllers/post.controller.js';
+import { create, getpost } from '../controllers/post.controller.js';
 
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create)
+router.get('/getposts', getpost)
 
 
 export default router;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
+import DashPost from '../components/DashPost';
 
 export default function Dashboard() {
 
@@ -16,13 +17,18 @@ export default function Dashboard() {
   }, [location.search]);
 
   return (
-    <div className="min-h-screen flex flex-col md:flex-row">
+    <div className='min-h-screen flex flex-col md:flex-row'>
       <div className='md:w-56'>
         {/* Sidebar */}
         <DashSidebar />
       </div>
+
       {/* profile... */}
       {tab === 'profile' && <DashProfile />}
+
+      {/* posts... */}
+      {tab === 'posts' && <DashPost />}
+
     </div>
   );
 }
